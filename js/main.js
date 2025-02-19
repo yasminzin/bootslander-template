4ac19c52260c896c844ecd3de227411fe118af0d
yasminzin <yasminaymanzin@gmail.com>
2025-02-20 00:17:46 +0200

    add some animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+window.scrollTo(0, 0);
+
 const loader = document.querySelector(".loader");
 const nav = document.querySelector(".navbar");
+// const numbers = document.querySelector(".number");
 
 setTimeout(() => {
   loader.style.transition = "opacity 0.3s ease";
@@ -9,6 +12,15 @@ setTimeout(() => {
   }, 400);
 }, 1000);
 
+// let countOne = 0;
+// countOneP.innerText = "0";
+
+// let countTwo = 0;
+// countTwoP.innerText = "0";
+
+// let countThree = 0;
+// countThreeP.innerText = "0";
+
 window.onscroll = function () {
   if (scrollY > 55) {
     nav.classList.add("sticky-bar");
@@ -16,4 +28,61 @@ window.onscroll = function () {
     nav.style.backgroundColor = "transparent";
     nav.classList.remove("sticky-bar");
   }
+  // if (scrollY > 900) {
+  //   var newInterval = setInterval(() => {
+  //     if (countOne == 4000) {
+  //       clearInterval(newInterval);
+  //       countOneP.innerText = `4,000+`;
+  //       return;
+  //     }
+  //     countOne++;
+  //     countOneP.innerText = `${countOne}+`;
+  //   }, 50);
+  //   var newInterval = setInterval(() => {
+  //     if (countTwo == 49) {
+  //       clearInterval(newInterval);
+  //       countTwoP.innerText = `49`;
+  //       return;
+  //     }
+  //     countTwo++;
+  //     countTwoP.innerText = `${countTwo}`;
+  //   }, 1000);
+  //   var newInterval = setInterval(() => {
+  //     if (countThree == 12) {
+  //       clearInterval(newInterval);
+  //       countThreeP.innerText = `12`;
+  //       return;
+  //     }
+  //     countThree++;
+  //     countThreeP.innerText = `${countThree}`;
+  //   }, 1000);
+  // } 
 };
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const elements = document.querySelectorAll('.forAnimate');
+console.log(elements)
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.7,
+});
+
+elements.forEach((element) => {
+  observer.observe(element);
+});
+
